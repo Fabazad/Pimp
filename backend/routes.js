c461a95eeae2970ce774444825846a5ad6268980
Fabien <fabien.turgut@etu.umontpellier.fr>
2019-08-27 06:52:30 +0200

    Update many route

--- backend/routes.js
+++ backend/routes.js
@@ -1,7 +1,7 @@
 const kittenRoutes = require('./routes/kitten.routes');
 
 async function routes (fastify) {
-    fastify.get("*", (request, reply) => {
+    fastify.get("/", (request, reply) => {
         reply.sendFile(__dirname = "../frontend/build/index.html")
     })
     kittenRoutes(fastify);
