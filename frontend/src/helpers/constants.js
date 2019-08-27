8230b0bb6c55fe6cb3514643854595ffc2cff7b2
Fabien <fabien.turgut@etu.umontpellier.fr>
2019-08-27 06:36:05 +0200

    change for /api/

--- frontend/src/helpers/constants.js
+++ frontend/src/helpers/constants.js
@@ -1,4 +1,4 @@
 export default {
-    SERVER_LOCAL_URL: 'http://127.0.0.1:8000',
-    SERVER_URL: 'https://tss-pimp.herokuapp.com'
+    SERVER_LOCAL_URL: 'http://127.0.0.1:8000/api',
+    SERVER_URL: 'https://tss-pimp.herokuapp.com/api'
 }
