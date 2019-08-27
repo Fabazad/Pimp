b2d6eb3424b908ea781500a2ab3cd9643509c2bd
Fabien <fabien.turgut@etu.umontpellier.fr>
2019-08-27 05:00:32 +0200

    Change frontend url

--- backend/helpers/constants.js
+++ backend/helpers/constants.js
@@ -1,6 +1,7 @@
 const constants = {
     MONGO_LOCAL_URL: 'mongodb://127.0.0.1:27017/pimp',
-    FRONTEND_LOCAL_URL: 'http://localhost:3000'
+    FRONTEND_LOCAL_URL: 'http://localhost:3000',
+    FRONTEND_URL: 'https://tss-pimp.herokuapp.com/'
 };
 
 module.exports = constants;
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const constants = require('./helpers/constants');
 const fastify = require('fastify')({ logger: true });
 
 fastify.register(require('fastify-cors'), { 
-  origin: constants.FRONTEND_LOCAL_URL
+  origin: constants.FRONTEND_URL
 });
 
 fastify.register(require('./routes'));
