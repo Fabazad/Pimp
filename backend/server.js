363b16b1cdcf6d08c9b4e79929d674a09c408efe
Fabien <fabien.turgut@etu.umontpellier.fr>
2019-08-27 05:24:32 +0200

    change port again

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ fastify.register(require('./db-connection'));
 
 // Run the server!
 const start = () => {
-  fastify.listen(process.env.PORT || 5000)
+  fastify.listen(process.env.PORT || '0.0.0.0')
   .catch (err => {
     fastify.log.error(err)
     process.exit(1)
